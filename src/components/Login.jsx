7a48890b9826927b2ff87679d33a8977162b6d14
Alexis Ballo <alexispballo@gmail.com>
2022-05-06 16:02:15 -0400

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,7 +3,7 @@ import { GoogleLogin } from "react-google-login";
 
 const clientID =
   "800283224875-mfp8r4rqffqm3bijtaqeac2m13g0tems.apps.googleusercontent.com";
-
+//test
 function Login() {
   const onSuccess = (res) => {
     console.log("Login Success: currentUser:", res.profileObj);
