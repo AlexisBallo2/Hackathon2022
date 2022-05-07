ce5b0c3275dd18c36d3d6dc19531b8f777a16e00
jpaul342 <jrpaul@middlebury.edu>
2022-05-07 10:28:41 -0400

    Changed Prompt
    none

--- src/components/AddNewItem.jsx
+++ src/components/AddNewItem.jsx
@@ -23,12 +23,12 @@ async function submitItem(i, pt, e, u, b, p) {
 function AddNewItem(props) {
   //this makes a new item into the database
   function call1() {
-    var i = prompt("item");
-    var pt = prompt("pt");
-    var et = prompt("et");
-    var u = prompt("user");
-    var bs = prompt("bsrb");
-    var p = prompt("price");
+    var i = prompt("Item/Service");
+    var pt = prompt("Payment Time");
+    var et = prompt("Entry Time");
+    var u = prompt("User ID");
+    var bs = prompt("Buy, Sell, or Borrow");
+    var p = prompt("Price");
     submitItem(i, pt, et, u, bs, p);
   }
 
