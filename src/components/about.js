d7f57529cd4d63524de5234d0be3d1c56d8a6e6b
terryluongo <trluongo@optimum.net>
2022-05-07 14:50:58 -0400

    Made about page

---
+++ src/components/about.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+import styles from "./about.module.css";
+
+
+const about = () => {
+  return (
+    <div className = {styles.aboutPage}> 
+    <title>About Us - Midd XChange</title>
+    <h1>About Midd XChange</h1>
+
+    </div>
+  );
+};
+
+export default about;
